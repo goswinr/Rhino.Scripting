2c069d34fd6a2a31845ff518a11be0fd298bc3ba
Goswin <gwinsky@yahoo.com>
2019-10-17 00:16:34 +0200

    trying pretty printer

--- src/19_ActiveDoc.fs
+++ src/19_ActiveDoc.fs
@@ -31,6 +31,81 @@ module ActiceDocument =
             //RhinoApp.EscapeKeyPressed.Add ( fun _ -> failwith "Esc Key was pressed, Exception raised via Rhino.Scripting")
 
 
+module internal NiceString=
+open System.Collections.Generic
+open System
+
+let maxDepth= 3
+let maxItemsPerSeq = 3
+
+let (|IsICollection|_|) (xs : obj) =
+    let interfaces= xs.GetType().GetInterfaces()
+    let ICollType = interfaces  |> Seq.tryFind( fun x -> x.IsGenericType && x.GetGenericTypeDefinition() = typedefof<ICollection<_>> )
+    match ICollType with
+    |Some t ->
+        let args = t.GetGenericArguments()
+        if args.Length = 1 then 
+            if args.[0].IsValueType then //create new trimmed Colllection with values boxed
+                let rs = ResizeArray<obj>()
+                let ics = xs :?> Collections.ICollection
+                let enum = ics.GetEnumerator()
+                let mutable k=0
+                while enum.MoveNext() && k < maxItemsPerSeq do
+                    rs.Add (box enum.Current)
+                    k <- k+1
+                Some (ics.Count,  rs :> ICollection<_>)                 
+            else                
+                let ics = xs :?> ICollection<_> 
+                Some (ics.Count, ics )
+        else
+            None
+    |None -> None
+    
+let sb = Text.StringBuilder()
+
+let add (s:string) =  s|> sb.Append |> ignore
+let addn (s:string) = s|> sb.AppendLine |> ignore
+
+let rec toNiceStringRec (x:'T,indent:int) =
+    //let ind  = System.String(' ', 4 * (indent-1))
+    let ind = System.String(' ', 4 *  indent )
+    
+    match box x with
+    | null -> "-null-"
+    | :? float      as v   -> v.ToString() |> addn
+    | :? single     as v   -> v.ToString() |> addn
+    | :? Char       as c   -> c.ToString() |> addn // "'" + c.ToString() + "'"
+    | :? string     as s   -> s |> addn // to not have it in quotes
+    | :? Guid       as g   -> sprintf "Guid[%O]" g |> addn
+    | IsICollection (count,xs) -> 
+                let sb = Text.StringBuilder()
+                //let ind  = System.String(' ', 4 * (indent-1))
+                //let indn = System.String(' ', 4 *  indent   )
+                //printfn "ind %d"  ind.Length
+                //printfn "indn %d" indn.Length
+                sprintf "%A with %d items:(%d indent)" xs count indent |> sb.AppendLine |> ignore        
+                if indent < maxDepth + 1 then 
+                    for x in xs  |> Seq.truncate maxItemsPerSeq do  
+                        //indn                             |> sb.Append      |> ignore
+                        let s = toNiceStringRec (x, indent+1)    
+                        sprintf "%A (%d indentt)" x  (indent+1) |> sb.AppendLine |> ignore    
+                    if count > maxItemsPerSeq then 
+                        //System.String(' ', 4*indent) |> sb.Append      |> ignore
+                        "..." |> sb.AppendLine  |> ignore
+                
+                sb.ToString()
+                
+    | _ ->  x.ToString()
+
+let toNiceString x = 
+    sb.Clear() |> ignore
+    toNiceStringRec(x,0)
+    sb.ToString()
+        
+printfn "%s" <| toNiceString [| [| [| 9;8;8;4;5;6|];[| 1;2;3|] |] ;[| [| 9;8;8;4;5|];[| 1;2|]|] |]
+
+printfn "%s" <| toNiceString 99.99
+
 module internal NiceString = 
     open System.Collections.Generic
     open Rhino.Geometry
@@ -39,14 +114,27 @@ module internal NiceString =
     let maxDepth= 3
     let maxItemsPerSeq = 3
 
-
     let (|IsICollection|_|) (xs : obj) =
         let interfaces= xs.GetType().GetInterfaces()
-        let hasIColl = interfaces  |> Seq.exists( fun x -> x.IsGenericType && x.GetGenericTypeDefinition() = typedefof<ICollection<_>> )
-        if hasIColl then 
-            Some ( xs :?> ICollection<_> )
-        else
-            None
+        let ICollType = interfaces  |> Seq.tryFind( fun x -> x.IsGenericType && x.GetGenericTypeDefinition() = typedefof<ICollection<_>> )
+        match ICollType with
+        |Some t ->
+            let args = t.GetGenericArguments()
+            if args.Length = 1 then 
+                if args.[0].IsValueType then //create new trimmed Colllection with values boxed
+                    let rs = ResizeArray<obj>()
+                    let ics = xs :?> Collections.ICollection
+                    let enum = ics.GetEnumerator()
+                    let mutable k=0
+                    while enum.MoveNext() && k < maxItemsPerSeq do
+                        rs.Add (box enum.Current)
+                        k <- k+1
+                    Some ( rs :> ICollection<_>)                 
+                else                
+                    Some ( xs :?> ICollection<_> )
+            else
+                None
+        |None -> None
         
 
     let rec  formatICollection (xs:ICollection<'T>,indent:int) =
@@ -76,7 +164,7 @@ module internal NiceString =
         | IsICollection xs -> formatICollection(xs,indent)
         | _ ->  x.ToString()
 
-    let toNiceString s = toNiceStringRec(s,0)
+    let toNiceString x = toNiceStringRec(x,0)
 
 module internal NiceStringNONREcusrive = 
     open System.Collections.Generic
