8292556a51e15b809fe305ec80045d4700bc78e9
Goswin <gwinsky@yahoo.com>
2019-10-21 12:04:58 +0200

    Removed all Block Comments

--- src/Linetype.fs
+++ src/Linetype.fs
@@ -16,17 +16,6 @@ module ExtensionsLinetype =
     ///<returns>(bool) True or False</returns>
     static member IsLinetype(name:string) : bool =
         notNull <| Doc.Linetypes.FindName(name)        
-    (*
-    def IsLinetype(name_or_id):
-        '''Verifies the existance of a linetype in the document
-        Parameters:
-          name_or_id (guid|str): The name or identifier of an existing linetype.
-        Returns: 
-          bool: True or False
-        '''
-        lt = __getlinetype(name_or_id)
-        return lt is not None
-    *)
 
 
     [<EXT>]
@@ -37,18 +26,6 @@ module ExtensionsLinetype =
         let lt = Doc.Linetypes.FindName(name)
         if isNull lt then failwithf "isLinetypeReference unable to find '%s' in a linetypes" name
         lt.IsReference
-    (*
-    def IsLinetypeReference(name_or_id):
-        '''Verifies that an existing linetype is from a reference file
-        Parameters:
-          name_or_id (guid|str): The name or identifier of an existing linetype.
-        Returns: 
-          bool: True or False
-        '''
-        lt = __getlinetype(name_or_id)
-        if lt is None: raise ValueError("unable to coerce %s into linetype"%name_or_id)
-        return lt.IsReference
-    *)
 
 
     [<EXT>]
@@ -56,14 +33,6 @@ module ExtensionsLinetype =
     ///<returns>(int) the number of linetypes in the document</returns>
     static member LinetypeCount() : int =
         Doc.Linetypes.Count
-    (*
-    def LinetypeCount():
-        '''Returns number of linetypes in the document
-        Returns:
-          number: the number of linetypes in the document
-        '''
-        return scriptcontext.doc.Linetypes.Count
-    *)
 
 
     [<EXT>]
@@ -76,21 +45,5 @@ module ExtensionsLinetype =
             let linetype = Doc.Linetypes.[i]
             if not linetype.IsDeleted then  rc.Add(linetype.Name)        
         rc
-    (*
-    def LinetypeNames(sort=False):
-        '''Returns names of all linetypes in the document
-        Parameters:
-          sort (bool, optional): return a sorted list of the linetype names
-        Returns:
-          list(str, ...): list of linetype names if successful
-        '''
-        count = scriptcontext.doc.Linetypes.Count
-        rc = []
-        for i in xrange(count):
-            linetype = scriptcontext.doc.Linetypes[i]
-            if not linetype.IsDeleted: rc.append(linetype.Name)
-        if sort: rc.sort()
-        return rc
-    *)
 
 
