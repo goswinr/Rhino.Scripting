182ad320b0f4a2aa3a2e7df701bfc432e00a75f1
Goswin <gwinsky@yahoo.com>
2019-12-06 11:51:58 +0100

    curried extraas

--- src/72_ExtrasCurried.fs
+++ src/72_ExtrasCurried.fs
@@ -11,50 +11,21 @@ open Microsoft.FSharp.Core.LanguagePrimitives
 open System.Runtime.CompilerServices // [<Extension>] Attribute not needed for intrinsic (same dll) type augmentations ?
 
 [<AutoOpen>]
-module ExtensionsCurve =
+module ExtrasCurried =
 
   //[<Extension>] //Error 3246
   type RhinoScriptSyntax with
 
     [<Extension>]
-    ///<summary>Modifies the layer of an object</summary>
+    ///<summary>Modifies the layer of an object, creates layer if not yet existing</summary>
     ///<param name="layer">(string) Name of an existing layer</param>
     ///<param name="objectId">(Guid) The identifier of the object</param>    
     ///<returns>(unit) void, nothing</returns>
-    static member setObjectLayer( layer:string) (objectId:Guid) : unit = 
-        let obj = RhinoScriptSyntax.CoerceRhinoObject(objectId)
-        let i = Doc.Layers.FindByFullPath(layer, RhinoMath.UnsetIntIndex)
-        
-        let layer = 
-            if i <> RhinoMath.UnsetIntIndex then 
-                Doc.Layers.[i]
-            else
-                let names = layer.Split([| "::"|],StringSplitOptions.RemoveEmptyEntries)
-                let mutable lastparentindex = -1
-                let mutable lastparent:DocObjects.Layer = null
-                for idx, name in Seq.indexed(names) do
-                  let layer = DocObjects.Layer.GetDefaultLayerProperties()
-                  if lastparentindex <> -1 then
-                      lastparent <- Doc.Layers.[lastparentindex]
-                  
-                  if notNull lastparent then
-                    layer.ParentLayerId <- lastparent.Id
-                  if notNull name then
-                    layer.Name <- name
-                  //color = RhinoScriptSyntax.Coercecolor(color)
-                  if not color.IsEmpty then layer.Color <- color
-                  layer.IsVisible <- visible
-                  layer.IsLocked <- locked
-                  lastparentindex <- Doc.Layers.Add(layer)
-                  if lastparentindex = -1 then
-                    let mutable fullpath = layer.Name
-                    if notNull lastparent then
-                        fullpath <- lastparent.FullPath + "::" + fullpath
-                    lastparentindex <- Doc.Layers.FindByFullPath(fullpath, RhinoMath.UnsetIntIndex)
-                Doc.Layers.[lastparentindex].FullPath
+    static member setLayer( layer:string) (objectId:Guid) : unit = //TODO optimise, used a lot
+        if not (RhinoScriptSyntax.IsLayer(layer)) then 
+            RhinoScriptSyntax.AddLayer(layer) |> ignore
+        RhinoScriptSyntax.ObjectLayer(objectId,layer)
 
-                
-        let index = layer.Index
-        obj.Attributes.LayerIndex <- index
-        if not <| obj.CommitChanges() then failwithf "Set ObjectLayer failed for '%A' and '%A'"  layer objectId
-        Doc.Views.Redraw()
+    static member setName( name:string) (objectId:Guid) : unit = RhinoScriptSyntax.ObjectName(objectId,name)    
+    
+    static member setUserText( key:string) ( value :string) (objectId:Guid) : unit = RhinoScriptSyntax.SetUserText(objectId,key,value)
