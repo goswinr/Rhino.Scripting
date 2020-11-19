df9ed579af67ba6af13512da82f8646b3e947ced
Goswin <gwinsky@yahoo.com>
2020-11-19 11:23:25 +0100

    UserInteractionException

--- Src/Synchronisation.fs
+++ Src/Synchronisation.fs
@@ -11,7 +11,9 @@ type RhinoScriptingException (s:string) =
     static member inline Raise msg = Printf.kprintf (fun s -> raise (new RhinoScriptingException(s))) msg 
     static member inline FailIfFalse s b = if not b then raise (new RhinoScriptingException(s))
 
-
+type UserInteractionException (s:string) =
+    inherit System.Exception(s)
+    static member inline Raise msg = Printf.kprintf (fun s -> raise (new UserInteractionException(s))) msg 
 
 /// A static classs to help access the UI thread from other threads
 type Synchronisation private () =
