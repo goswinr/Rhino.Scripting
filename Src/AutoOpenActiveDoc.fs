076dcf2b16842a0be44e91001c7d6ff43453e1db
Goswin <gwinsky@yahoo.com>
2020-11-19 21:00:25 +0100

    fix bug in Ot variable

--- Src/AutoOpenActiveDoc.fs
+++ Src/AutoOpenActiveDoc.fs
@@ -21,21 +21,23 @@ module AutoOpenActiveDocument =
     /// The current active Rhino document (= the file currently open)
     let mutable Doc = 
         if HostUtils.RunningInRhino then
-            Rhino.RhinoDoc.ActiveDoc 
+            Rhino.RhinoDoc.ActiveDoc  // will be reset in EndOpenDocument Event
         else 
             failwith "failed to find the active Rhino document, is this dll running hosted inside the Rhino process? " 
     
     /// Object Table of the current active Rhino documents
-    let Ot = Doc.Objects
+    let mutable Ot = Doc.Objects  // will be reset in EndOpenDocument Event
     
     //------------------------------------------------------------------------------------
     // --------------- all below values and functions are internal : ------------------
     //------------------------------------------------------------------------------------
-    
+    let mutable internal escapePressed = false // will be reset in EndOpenDocument Event
 
     /// To store last created object form executing a rs.Command(...)
-    let mutable internal commandSerialNumbers : option<uint32*uint32> = None 
+    let mutable internal commandSerialNumbers : option<uint32*uint32> = None // will be reset in EndOpenDocument Event
        
+
+
     /// Gets a localised descritipn on rhino object type (curve , point, surface ....)
     let internal rhType (g:Guid)=
         if g = Guid.Empty then "-Guid.Empty-"
@@ -52,7 +54,7 @@ module AutoOpenActiveDocument =
     
 
     /// So that python range expressions dont need top be translated to F#
-    let internal range(l) = seq{0..(l-1)} 
+    let internal range(l) = seq{ 0 .. (l-1) } 
 
     /// If first value is 0.0 return second else first
     let internal ifZero1 a b = if a = 0.0 then b else a
@@ -60,27 +62,34 @@ module AutoOpenActiveDocument =
     /// if second value is 0.0 return first else second
     let internal ifZero2 a b = if b = 0.0 then a else b    
 
-    let mutable internal escapePressed = false
-    
+        
     do
         if HostUtils.RunningInRhino then
             Synchronisation.Initialize() //declared in Synchronisation static class
             
             let setup() = 
                 // keep the reference to the active Document (3d file ) updated.
-                Rhino.RhinoDoc.EndOpenDocument.Add (fun args -> Doc <- args.Document)
+                Rhino.RhinoDoc.EndOpenDocument.Add (fun args -> 
+                    Doc <- args.Document
+                    Ot  <- args.Document.Objects
+                    commandSerialNumbers <- None
+                    escapePressed <- false
+                    )
                 
                 // listen to Esc Key press.
                 // doing this "Add" in sync is only required if no handler has been added in sync before. Adding the first handler to this from async thread cause a Access violation exeption that can only be seen with the window event log.
                 // This his handler does not work on Sync evaluationmode , TODO: test!               
-                RhinoApp.EscapeKeyPressed.Add    ( fun _    -> if not escapePressed  &&  not <| Input.RhinoGet.InGet(Doc) then escapePressed <- true) 
+                RhinoApp.EscapeKeyPressed.Add    ( fun _ -> 
+                    if not escapePressed  &&  not <| Input.RhinoGet.InGet(Doc) then 
+                        escapePressed <- true
+                    ) 
                 
             
             if RhinoApp.InvokeRequired then 
                 if isNull Synchronisation.SyncContext then                    
                     "Synchronisation.syncContext could not be set via reflection.\r\n"+
                     "Rhino.Scripting.dll is not loaded from main thread \r\n"+
-                    "it need to set up callbacks for pressing Esc key and changing the active document. \r\n"+
+                    "it needs to set up callbacks for pressing Esc key and changing the active document. \r\n"+
                     "Setting up these event handlers async can trigger a fatal access violation exception \r\n"+
                     "if its the first time you access the event.\r\n"+
                     "Try to set them up yourself on main thread \r\n"+
@@ -89,7 +98,7 @@ module AutoOpenActiveDocument =
                     "RhinoApp.EscapeKeyPressed.Add     ( fun _    ->  \r\n"+
                     "          if not escapePressed  &&  not <| Input.RhinoGet.InGet(Doc) then escapePressed <- true)" 
                     |>! RhinoApp.WriteLine 
-                    |> printfn "%s"  
+                    |> eprintfn "%s"  
                 else
                     async{ 
                         do! Async.SwitchToContext Synchronisation.SyncContext // ensure its done on UI thread
