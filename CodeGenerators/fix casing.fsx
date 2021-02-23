bf9856e184e0b2ac2014c2ae2ab33281c3f02c92
Goswin <gwinsky@yahoo.com>
2021-02-23 16:00:24 +0100

    Update fix casing.fsx

--- CodeGenerators/fix casing.fsx
+++ CodeGenerators/fix casing.fsx
@@ -6,23 +6,35 @@ open FsEx
 let folder = @"D:\Git\Rhino.Scripting\Src\"
 let files = IO.getAllFilesByPattern folder "*.fs" |> Rarr.ofSeq
 
-
-let prevName,newName = " rhino" ,  " Rhino"
+//localised
+let prevName,newName = " curve" ,  " Curve"
 
 
 for f in files do
     let shortPath = f.Replace(folder, "") 
-    printfnColor 0 170 0 "%s:" shortPath
+    printfnColor 255 110 255 "%s:" shortPath
     let lines = IO.File.ReadAllLines(f)
+    let mutable any  = false
     let code = stringBuffer{ 
         for ln in lines do    
             let t = ln.Trim() 
             if t.StartsWith "///" then  
                 if t.Contains prevName then 
-                    //printfn "%s" ln
                     printWithHighlight prevName ln
+                    yield! ln.Replace(prevName, newName)
+                    any <- true
+                else 
+                    yield! ln
+            else 
+                yield! ln
                     
         }
-    () 
+    if true   && any then     
+        if code.EndsWith(Environment.NewLine) then 
+            IO.File.WriteAllText(f, code) 
+        else 
+            IO.File.WriteAllText(f, code + Environment.NewLine) 
+    
+     
         
           
