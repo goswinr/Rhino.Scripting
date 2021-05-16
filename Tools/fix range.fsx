4cf7454166e9a9b832c9ef5bfa8ba2d81c36ef48
Goswin <gwinsky@yahoo.com>
2021-05-16 10:01:28 +0200

    Update fix range.fsx

--- Tools/fix range.fsx
+++ Tools/fix range.fsx
@@ -7,26 +7,30 @@ let dir = @"C:\GitHub\Rhino.Scripting\Src"
 
 let doFixAttr (path) = 
     //printfn "doing %s" path 
-    //rarr{
-    for ln in IO.File.ReadAllLines(path)  do 
-        
-        if ln.Contains "Util.range" then
-            let p, t, n = String.splitTwice "(" ")" ln
-         
-            printWithHighlight "Util.range" ln
+    rarr{
+        for ln in IO.File.ReadAllLines(path)  do 
             
-            let  a = ""
-            () 
-            
-        else 
-            //yield ln
-            () 
-           
-        //}
+            if ln.Contains "Util.range" then
+                let p, t, n = String.splitTwice "(" ")" ln
+             
+                printWithHighlight "Util.range" ln
+                
+                let  a = p.Replace("in Util.range", "= 0 to ") 
+                let b =  t + " - 1"
+                let n = a + b + n
+                printWithHighlight "= 0 to " n
+                printfn ""
+                yield n
+                
+            else 
+                yield ln
+                () 
+        //yield ""       
+        }
   
                 
     //|>! Seq.iter (printfn "%s") 
-    //|> fun xs -> IO.File.WriteAllLines(path, xs) 
+    |> fun xs -> IO.File.WriteAllLines(path, xs) 
     
     
     
