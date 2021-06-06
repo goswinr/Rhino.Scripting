0322648f6f00d3b8387b6af9527dfdd2b3ecdad8
Goswin <gwinsky@yahoo.com>
2021-06-06 10:09:26 +0200

    doc string

--- Src/RhinoScriptSyntax/Document.fs
+++ Src/RhinoScriptSyntax/Document.fs
@@ -686,23 +686,24 @@ module ExtensionsDocument =
     ///    7 - Mils (2.54e-5 meters, 0.001 inches)
     ///    8 - Inches (0.0254 meters)
     ///    9 - Feet (0.3408 meters, 12 inches)
-    ///      10 - Miles (1609.344 meters, 5280 feet)
-    ///      11 - *Reserved for custom Unit System*
-    ///      12 - Angstroms (1.0e-10 meters)
-    ///      13 - Nanometers (1.0e-9 meters)
-    ///      14 - Decimeters (1.0e-1 meters)
-    ///      15 - Dekameters (1.0e + 1 meters)
-    ///      16 - Hectometers (1.0e + 2 meters)
-    ///      17 - Megameters (1.0e + 6 meters)
-    ///      18 - Gigameters (1.0e + 9 meters)
-    ///      19 - Yards (0.9144  meters, 36 inches)
-    ///      20 - Printer point (1/72 inches, computer points)
-    ///      21 - Printer pica (1/6 inches, (computer picas)
-    ///      22 - Nautical mile (1852 meters)
-    ///      23 - Astronomical (1.4959787e + 11)
-    ///      24 - Lightyears (9.46073e + 15 meters)
-    ///      25 - Parsecs (3.08567758e + 16)</param>
-    ///<param name="scale">(bool) Scale existing geometry based on the new unit system.
+    ///   10 - Miles (1609.344 meters, 5280 feet)
+    ///   11 - *Reserved for custom Unit System*
+    ///   12 - Angstroms (1.0e-10 meters)
+    ///   13 - Nanometers (1.0e-9 meters)
+    ///   14 - Decimeters (1.0e-1 meters)
+    ///   15 - Dekameters (1.0e + 1 meters)
+    ///   16 - Hectometers (1.0e + 2 meters)
+    ///   17 - Megameters (1.0e + 6 meters)
+    ///   18 - Gigameters (1.0e + 9 meters)
+    ///   19 - Yards (0.9144  meters, 36 inches)
+    ///   20 - Printer point (1/72 inches, computer points)
+    ///   21 - Printer pica (1/6 inches, (computer picas)
+    ///   22 - Nautical mile (1852 meters)
+    ///   23 - Astronomical (1.4959787e + 11)
+    ///   24 - Lightyears (9.46073e + 15 meters)
+    ///   25 - Parsecs (3.08567758e + 16)</param>
+    ///<param name="scale">(bool) Optional, Default Value: <c>false</c>
+    ///    Scale existing geometry based on the new unit system.
     ///    If not specified, any existing geometry is not scaled (False)</param>
     ///<returns>(unit) void, nothing.</returns>
     [<Extension>]
